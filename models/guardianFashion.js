99a4dcf7bc52edcc8632e64c3f62437a191ed1f0
Anthony <ajsur1017@gmail.com>
2021-10-09 14:14:14 -0400

    fixed user auth issue

--- models/guardianFashion.js
+++ models/guardianFashion.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose
 
 const guardianLoadoutSchema = new Schema(
     {
-        helmOrnament: String, helmShader: String, gloveOrnament: String, gloveShader: String, chestOrnament: String, chestShader: String, bootOrnament: String, bootShader: String, classItemOrnament: String, classItemShader: String, fashionName: String, user: String
+        helmOrnament: String, helmShader: String, gloveOrnament: String, gloveShader: String, chestOrnament: String, chestShader: String, bootOrnament: String, bootShader: String, classItemOrnament: String, classItemShader: String, fashionName: String, username: String
     }
 )
 const GuardianFashion = model("GuardianFashion", guardianLoadoutSchema)
