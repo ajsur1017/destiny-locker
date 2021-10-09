aad8daf647ea70bc33fd808a988540588cdb4941
Anthony <ajsur1017@gmail.com>
2021-10-09 19:43:28 -0400

    updated for alex

--- controllers/guardian.js
+++ controllers/guardian.js
@@ -12,32 +12,35 @@ router.use((req, res, next) => {
         res.redirect("/user/login")
     }
 })
-
+// Main route to primary index
 router.get("/", (req, res) => {
     res.render("guardian/index.ejs")
 })
+// Route to loadout index
 router.get("/indexLoadout", (req, res) => {
     Guardian.find({ username: req.session.username }, (err, guardian) => {
         res.render("guardian/indexLoadout.ejs", { guardian })
     })
 })
+// Route to fashion index
 router.get("/indexFashion", (req, res) => {
     GuardianFashion.find({ username: req.session.username }, (err, guardianFashion) => {
         res.render("guardian/indexFashion.ejs", { guardianFashion })
     })
 })
+// Route to new loadout
 router.get("/newLoadout", (req, res) => {
     res.render("guardian/newLoadout.ejs")
 })
+// Route to new Fashion
 router.get("/newFashion", (req, res) => {
     res.render("guardian/newFashion.ejs")
 })
 
-router.post("/newFashion", (req, res) => {
-    // req.body.readyToEat = req.body.readyToEat === "on" ? true : false
+router.post("/indexFashion", (req, res) => {
     req.body.username = req.session.username
-    Guardian.create(req.body, (err, guardian) => {
-        res.redirect("/guardian/indexFashion.ejs")
+    GuardianFashion.create(req.body, (err, guardianFashion) => {
+        res.redirect("guardian/indexFashion.ejs")
     })
 })
 
@@ -50,7 +53,6 @@ router.get("/:id/edit", (req, res) => {
 
 router.put("/:id", (req, res) => {
     const id = req.params.id
-    // req.body.readyToEat = req.body.readyToEat === "on" ? true : false
     Guardian.findByIdAndUpdate(id, req.body, { new: true }, (err, guardian) => {
         res.redirect("/guardian")
     })
